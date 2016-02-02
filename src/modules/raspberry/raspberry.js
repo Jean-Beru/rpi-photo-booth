fcd9d459af75f17b77e3e43dc56f20870f4a0c3e
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2016-02-02 21:07:58 +0100

    Set lower stream quality

--- src/modules/raspberry/raspberry.js
+++ src/modules/raspberry/raspberry.js
@@ -5,7 +5,7 @@ var options = {
         preview: {
             mode:      'timelapse',
             output:    process.cwd() + '/stream/caption.jpg',
-            quality:   5,
+            quality:   1,
             width:     640,
             height:    480,
             timelapse: 300,
